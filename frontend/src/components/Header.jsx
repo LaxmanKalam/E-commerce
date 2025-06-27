031fe9eb4564b0fd15cae216bdcff83cb0c8cd67
Laxman kalam <laxmankalam88321@gmail.com>
2025-06-28 00:46:48 +0530

    update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,43 @@
 import React from 'react'
 import Logo from './Logo'
-import { CiSearch } from "react-icons/ci";
+import { GrSearch } from "react-icons/gr";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='h-20 shadow-md'>
+    <header className='h-20 shadow-md bg-white'>
       <div className='h-full container mx-auto flex items-center px-4 justify-between'>
         <div className='ml-0'>
+          <Link to={"/"}>
             <Logo w={80} h={70} />
+          </Link>
+        </div>
+        {/* by default hidden in mobile, show in desktop */}
+        <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
+          <input type="search" placeholder='search product...' className='w-full outline-none' />
+          <div className='text-lg min-w-[50px] h-8  flex items-center justify-center rounded-r-full '>
+            <GrSearch />
+          </div>
         </div>
 
-        <div>
-           <input type="search" placeholder='search producet...' />
-           <div>
-            <CiSearch/>
-           </div>
-        </div>
+        <div className='flex items-center gap-7'>
 
-        <div>
-          user icon and cards
+          <div className='text-3xl cursor-pointer'>
+            <FaRegCircleUser />
+          </div>
+
+          <div className='text-2xl relative '>
+            <span><FaShoppingCart /></span>
+            <div className='bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-2'>
+              <p className='text-sm '>0</p>
+            </div>
+          </div>
+
+          <div>
+            <Link to={"/login"} className='bg-red-600 text-white px-3 py-1 rounded-full hover:bg-red-700'>Login</Link>
+          </div>
         </div>
       </div>
     </header>
--- frontend/src/routes/main.jsx
+++ frontend/src/routes/main.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import App from '../App'
 import Home from '../pages/Home.jsx';
+import Login from '../pages/Login.jsx';
 
 const router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
             {
                 path: "",
                 element: <Home />
+            },
+            {
+                path:"login",
+                element:<Login/>
             }
         ]
     }
