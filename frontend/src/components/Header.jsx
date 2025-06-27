d5d1e3b622dfb3a3e49e8b27fa407295610ac4e7
Laxman kalam <laxmankalam88321@gmail.com>
2025-06-27 22:00:48 +0530

    update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Logo from './Logo'
+import { CiSearch } from "react-icons/ci";
 
 const Header = () => {
   return (
-    <header className='h-16 shadow-md'>
-      <div className='container mx-auto'>
-        <div className=''>
-            <Logo w={100} h={60} />
+    <header className='h-20 shadow-md'>
+      <div className='h-full container mx-auto flex items-center px-4 justify-between'>
+        <div className='ml-0'>
+            <Logo w={80} h={70} />
+        </div>
+
+        <div>
+           <input type="search" placeholder='search producet...' />
+           <div>
+            <CiSearch/>
+           </div>
+        </div>
+
+        <div>
+          user icon and cards
         </div>
       </div>
     </header>
